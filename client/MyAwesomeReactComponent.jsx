a9d13ef2a286e0973510d85c49b97bd58b79ffd4
Audax <audax.anchirinah@andela.com>
2017-06-19 18:18:16 +0100

    chore(#146922329): fixed test coverage script [delivers #146922329]

--- client/MyAwesomeReactComponent.jsx
+++ client/MyAwesomeReactComponent.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
 
-const MyAwesomeReactComponent = () => (
-  <RaisedButton label="Default" />
+const Main = () => (
+  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+    <AppBar title="My AppBar" />
+  </MuiThemeProvider>
 );
 
-export default MyAwesomeReactComponent;
+export default Main;
